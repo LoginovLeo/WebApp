1f0e9ca464efd76ac8922edafa5676383c9fa687
LoginovLeo <loginov.l93@gmail.com>
2021-05-06 18:42:45 +0300

    Some Changes

--- src/main/webapp/DBService/DBService.java
+++ src/main/webapp/DBService/DBService.java
@@ -1,4 +1,8 @@
 package main.webapp.DBService;
 
 public class DBService {
+
+    static final String DB_URL ="jdbc:postgresql://127.0.0.1:5432/simpleWS";
+    static final String LOGIN = "postgres";
+    static final String PASS = "1111";
 }
