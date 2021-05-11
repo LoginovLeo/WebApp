fd64faa32f17f56c5a247cf5c3542b04010eefd8
LoginovLeo <loginov.l93@gmail.com>
2021-05-11 03:02:37 +0300

    Changes

--- src/main/java/controller/GetMessage.java
+++ src/main/java/controller/GetMessage.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.Writer;
 import java.util.List;
 
 public class GetMessage extends HttpServlet {
